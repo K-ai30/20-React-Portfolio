e10cd930a57354cfb661ff65ac5a67cca13b022f
K-ai30 <artpirate11@aim.com>
2020-04-25 17:11:52 -0400

    added header and footer components

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FooterNav from './components/Footer/Footer';
 import HeaderNav from './components/Header/Header';
+import CodeImage from './components/CodeImage/CodeImage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <HeaderNav />
+      <CodeImage />
       <FooterNav />
     </div>
   )
--- reactportfolio/src/components/CodeImage/CodeImage.js
+++ reactportfolio/src/components/CodeImage/CodeImage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./CodeImage.css";
 
+// var code = require('../../../src/images/');
+
 function PurpleCode() {
     return (
-        <div class="parallax1">
-            <h1 class="d-flex">ABOUT</h1>
+        <div className="parallax1">
+            <h1 className="d-flex">ABOUT</h1>
         </div>
     )
 }
+
+// console.log(code);
 
 export default PurpleCode;
